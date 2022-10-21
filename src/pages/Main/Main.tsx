76ff2ac1c154264ff8db78527e91c92ed1cb5410
Polina Golovanova <polin-mart@ya.ru>
2022-10-22 00:28:59 +0300

    Add routing

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,10 @@
+// @flow
+
+import React, {FC} from 'react';
+
+export const Main: FC = () => {
+
+	return (
+		<div className='main'>MAIN PAGE</div>
+	);
+};
---
+++ src/pages/SingleStory/SingleStory.tsx
@@ -0,0 +1,10 @@
+// @flow
+
+import React, {FC} from 'react';
+
+export const SingleStory: FC = () => {
+
+	return (
+		<div className='main'>SINGLE STORY PAGE</div>
+	);
+};
