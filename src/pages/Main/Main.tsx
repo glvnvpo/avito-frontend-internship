2d481c65e025bef27c4c32c342906aa07b28f065
Polina Golovanova <polin-mart@ya.ru>
2022-10-23 19:21:47 +0300

    Add rendering data in Main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,12 +4,16 @@ import React, {FC, useEffect, useState, MouseEvent} from 'react';
 import {useLocation} from 'react-router-dom';
 import {Button} from 'antd';
 import axios from 'axios';
-import {isNull} from 'lodash';
+import {isEmpty, isNull} from 'lodash';
+import './styles.scss';
 import {ID, Story} from '../../types';
+import {StoryCard} from '../../components/StoryCard';
+import {Spinner} from '../../components/Spinner';
 import {setStories} from '../../store/stories';
 import {MINUTE} from '../../constants/time';
 import {ITEM, NEW_STORIES} from '../../api/constants';
 import {useAppDispatch, useAppSelector} from '../../hooks/redux';
+import {MAIN_PAGE_PATH} from '../../routing/constants';
 
 const STORIES_NUMBER = 100;
 
@@ -60,12 +64,12 @@ export const Main: FC = () => {
 						setLoading(false);
 					});
 			})
-			.catch((err) => console.error(err));
+			.catch(() => setLoading(false));
 	};
 
 	const loadOneStory = (id: ID) => {
-		return new Promise((resolve, reject)=> {
-			axios(ITEM(id))
+		return new Promise((resolve, reject) => {
+			axios<Story>(ITEM(id))
 				.then(({data}) => !isNull(data) ? resolve(data) : reject())
 				.catch(err => reject(err));
 		});
@@ -75,10 +79,22 @@ export const Main: FC = () => {
 		<div className='main'>
 			<div className='content'>
 				<div className='header bg-white'>
-					<h4 className='color-grey2'>Latest news</h4>
+					<span className='title bold color-grey2'>Latest news</span>
 					<Button onClick={(e) => loadNewStories(e)} type='primary'>Update news</Button>
 				</div>
 
+				<div className='cards mt-10'>
+					{ isLoading ? <Spinner className='mt-20'/> :
+						(!isEmpty(stories)) ? stories.map((story: Story) =>
+							<StoryCard
+								story={story}
+								key={story.id}
+								asLink
+								to={`${MAIN_PAGE_PATH}/${story.id}`}
+							/>
+						) : <span className='empty mt-20 bold color-dark-grey'>No stories found :(</span>
+					}
+				</div>
 			</div>
 		</div>
 	);
