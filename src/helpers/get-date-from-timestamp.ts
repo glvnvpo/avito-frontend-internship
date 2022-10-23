e6d3132966c1580092bd0496a2cb39026d6294bd
Polina Golovanova <polin-mart@ya.ru>
2022-10-23 20:44:21 +0300

    Update StoryCard component layout

---
+++ src/helpers/get-date-from-timestamp.ts
@@ -0,0 +1,12 @@
+// @flow
+
+export const getDateFromTimestamp = (timestamp: string | number): string => {
+
+	const date = new Date(Number(timestamp) * 1000);
+
+	const day = date.getDate();
+	const month = date.getMonth() + 1;
+	const year = date.getFullYear();
+
+	return `${day < 10 ? '0' : ''}${day}.${month < 10 ? '0' : ''}${month}.${year}`;
+};
