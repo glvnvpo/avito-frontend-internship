107c9054f75422bed85917b69cfb92ea9d0ea75a
Polina Golovanova <polin-mart@ya.ru>
2022-10-23 20:44:21 +0300

    Update StoryCard component layout

--- src/components/StoryCard/story-card.tsx
+++ src/components/StoryCard/story-card.tsx
@@ -1,7 +1,11 @@
 // @flow
 
-import React, {FC} from 'react';
+import React, {FC, HTMLAttributes} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {Card} from 'antd';
+import './styles.scss';
 import {Story} from '../../types';
+import {getDateFromTimestamp} from '../../helpers/get-date-from-timestamp';
 
 enum Fields {
     ALL= 'all',
@@ -18,10 +22,12 @@ type Props = {
     fieldsToShow?: Array<Fields>;
 }
 
-export const StoryCard: FC<Props> = ({story, isLoading=false, asLink=false, 
+export const StoryCard: FC<Props & HTMLAttributes<any>> = ({story, isLoading=false, asLink=false,
 	to, fieldsToShow=[Fields.ALL], ...rest}) => {
     
 	let {title, score, by, time, url, text, descendants} = story;
+
+	const navigate = useNavigate();
 
 	const shouldShowField = (key: Fields): boolean => {
 		if (fieldsToShow.includes(Fields.ALL)) {
@@ -30,10 +36,18 @@ export const StoryCard: FC<Props> = ({story, isLoading=false, asLink=false,
 
 		return fieldsToShow.includes(key);
 	};
+	
+	const navigateTo = (path: string | undefined): void => {
+		if (path) {
+			navigate(path);
+		}
+	};
 
 	return (
-		<div className='story'>
-			{title}
-		</div>
+		<Card title={title} className={`story color-grey3 ${asLink && 'link'}`}
+			  onClick={() => navigateTo(to)} {...rest}>
+			{score} points | <span className='user'>{by}</span> | {getDateFromTimestamp(time)}
+		</Card>
+
 	);
 };
