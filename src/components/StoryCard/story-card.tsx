6faa99308fb5d9f61e8007e473952a5982115306
Polina Golovanova <polin-mart@ya.ru>
2022-10-24 14:00:07 +0300

    Update StoryCard component to show more fields

--- src/components/StoryCard/story-card.tsx
+++ src/components/StoryCard/story-card.tsx
@@ -7,34 +7,34 @@ import './styles.scss';
 import {Story} from '../../types';
 import {getDateFromTimestamp} from '../../helpers/get-date-from-timestamp';
 
-enum Fields {
+export enum Fields {
     ALL= 'all',
     URL = 'url',
-    DESCENDANTS = 'descendants',
-    TEXT = 'text'
+    COMMENTS_COUNT = 'comments-count',
+	SCORE = 'score'
 }
 
 type Props = {
-    story: Story | undefined;
+    story?: Story;
     isLoading?: boolean;
     asLink?: boolean;
     to?: string;
-    fieldsToShow?: Array<Fields>;
+	extraFieldsToShow?: Array<Fields>;
 }
 
 export const StoryCard: FC<Props & HTMLAttributes<any>> = ({story, isLoading=false, asLink=false,
-	to, fieldsToShow=[Fields.ALL], ...rest}) => {
+	to, extraFieldsToShow=[Fields.ALL], ...rest}) => {
     
-	let {title, score, by, time, url, text, descendants} = story || {};
+	let {title, score, by, time, url, descendants} = story || {};
 
 	const navigate = useNavigate();
 
-	const shouldShowField = (key: Fields): boolean => {
-		if (fieldsToShow.includes(Fields.ALL)) {
+	const shouldShowExtraField = (key: Fields): boolean => {
+		if (extraFieldsToShow.includes(Fields.ALL)) {
 			return true;
 		}
 
-		return fieldsToShow.includes(key);
+		return extraFieldsToShow.includes(key);
 	};
 	
 	const navigateTo = (path: string | undefined): void => {
@@ -46,7 +46,19 @@ export const StoryCard: FC<Props & HTMLAttributes<any>> = ({story, isLoading=fal
 	return (
 		<Card title={title} className={`story color-grey3 ${asLink && 'link'}`}
 			  onClick={() => navigateTo(to)} {...rest}>
-			{score}&nbsp;points&nbsp;| <span className='user'>{by}</span>&nbsp;| {getDateFromTimestamp(time)}
+			<span>
+				{ shouldShowExtraField(Fields.SCORE) && <>{score}&nbsp;point{(score && score>1) &&'s'}&nbsp;| </> }
+				<span className='user'>{by}</span>&nbsp;| {getDateFromTimestamp(time)}</span>
+
+			{
+				shouldShowExtraField(Fields.URL) &&
+				<span>{url ? <a href={url} target='_blank' rel='noreferrer'>Visit source</a> : 'No source link available'}</span>
+			}
+
+			{
+				shouldShowExtraField(Fields.COMMENTS_COUNT) &&
+				<span>Comments count: {descendants}</span>
+			}
 		</Card>
 
 	);
