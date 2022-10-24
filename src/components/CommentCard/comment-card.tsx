003b3624bb683b3d8e2297fa652a3067ca515666
Polina Golovanova <polin-mart@ya.ru>
2022-10-24 16:50:51 +0300

    Add Show answers button

--- src/components/CommentCard/comment-card.tsx
+++ src/components/CommentCard/comment-card.tsx
@@ -1,7 +1,8 @@
 // @flow
 
 import React, {FC, ReactNode} from 'react';
-import {Comment} from 'antd';
+import {Button, Comment} from 'antd';
+import {isEmpty} from 'lodash';
 import parse from 'html-react-parser';
 import './styles.scss';
 import {Comment as CommentType} from '../../types';
@@ -10,7 +11,7 @@ import {getDateFromTimestamp} from '../../helpers/get-date-from-timestamp';
 type Props = {
 	comment: CommentType;
 	isParent?: boolean;
-	showAnswers?: () => void;
+	showAnswers?: (parentComment: CommentType) => void;
 	children?: ReactNode;
 }
 
@@ -18,8 +19,20 @@ export const CommentCard: FC<Props> = ({comment, isParent=true, showAnswers, chi
 
 	let {by, time, text, kids, isLoadingChildren, showChildComment} = comment || {};
 
+	const getBtnText = (isOpen: boolean | undefined):string => {
+		if (isOpen) {
+			return 'Hide answers';
+		}
+		else return 'Show answers';
+	};
+
+	const actions = [
+		(isParent && !isEmpty(kids) && showAnswers)
+			&& <Button onClick={() => showAnswers(comment)}>{getBtnText(showChildComment)}</Button>
+	];
+
 	return (
-		<Comment className='comment-card' author={by} content={parse(text)} datetime={getDateFromTimestamp(time)}>
+		<Comment className='comment-card' author={by} content={parse(text)} datetime={getDateFromTimestamp(time)} actions={actions}>
 			{children}
 		</Comment>
 	);
